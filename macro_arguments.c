49cb31d068e20cd6058ae35edd65c407c6422510
Lakshminarayanan0998 <lakshminara140@gmail.com>
2025-02-03 15:24:03 +0530

    Add files via upload

---
+++ macro_arguments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+#define SQUARE(x) ((x) * (x))
+#define CUBE(x) ((x) * (x) *(x))
+
+int main()
+{
+    int num=5;
+    printf("Square of %d is %d\n",num,SQUARE(num));
+    printf("Cube of %d is %d\n",num,CUBE(num));
+
+    return 0;
+}
