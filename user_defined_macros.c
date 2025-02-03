7215cdfc1b6c9822de8d1bccaf6b23ac485b795c
Lakshminarayanan0998 <lakshminara140@gmail.com>
2025-02-03 15:15:51 +0530

    Add files via upload

---
+++ user_defined_macros.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("File Name = %s\n",__FILE__);
+    printf("Date = %s\n",__DATE__);
+    printf("Time = %s\n",__TIME__);
+    printf("Line Number = %d\n",__LINE__);
+    printf("STDC = %d\n",__STDC__);
+    printf("Line Number = %d\n",__LINE__);
+
+    return 0;
+}
